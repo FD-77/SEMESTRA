a6969f4e486a3162a5db6badbbb409e2b92ebeac
WenAlgo <cwu009@citymail.cuny.edu>
2025-04-15 17:34:24 -0400

    Add Footer Component

--- semestra/src/components/Footer.jsx
+++ semestra/src/components/Footer.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 const Footer = () => {
-    return <footer>Footer</footer>;
+    return (
+        <footer className="bg-purple-300 text-xl text-white py-10">
+            <p>Powered by Semestra</p>
+            <p>
+                Copyright &copy; 2025 All Rights Reserved By Sean Jenkins,
+                Cuiwen Wu, Emmanuelle Padilla, Fatoumata Drammeh
+            </p>
+        </footer>
+    );
 };
 
 export default Footer;
