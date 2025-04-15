5dec8662a4992d71e2b13497f16c6a85860ac8b3
WenAlgo <cwu009@citymail.cuny.edu>
2025-04-15 10:59:50 -0400

    Add Navigation Bar CSS Style

--- semestra/src/components/Navbar.jsx
+++ semestra/src/components/Navbar.jsx
@@ -6,28 +6,37 @@ import { TbCalendarTime } from "react-icons/tb";
 import { ImCalculator } from "react-icons/im";
 import { FaRegUser } from "react-icons/fa";
 
+const navigation = [
+    { name: "CLASSES", href: "/classes", icon: TbCalendarTime },
+    { name: "GPA Calculator", href: "/gpaCalculator", icon: ImCalculator },
+    { name: "Profile", href: "/profile", icon: FaRegUser },
+];
+
 const Navbar = () => {
     return (
         <header>
-            <nav className="flex justify-between">
+            <nav className="flex justify-between items-center">
                 <div>
-                    <Link to="/">
-                        <GiThreeLeaves />
-                        <span>SEMESTRA</span>
+                    <Link to="/" className="flex items-center space-x-2">
+                        <GiThreeLeaves className="inline-block size-12 text-purple-300" />
+                        <span className="text-3xl font-bold font-mono">
+                            SEMESTRA
+                        </span>
                     </Link>
                 </div>
-                <div>
-                    <TbCalendarTime />
-                    <Link to="/classes">CLASSES</Link>
-                </div>
-                <div>
-                    <ImCalculator />
-                    <Link to="/gpaCalculator">GPA Calculator</Link>
-                </div>
-                <div>
-                    <FaRegUser />
-                    <Link to="/profile">Profile</Link>
-                </div>
+                {navigation.map((item) => (
+                    <div key={item.name}>
+                        <Link
+                            to={item.href}
+                            className="flex items-center space-x-2 px-6 py-4 bg-purple-300 text-white rounded-full hover:bg-purple-400"
+                        >
+                            <item.icon className="inline-block size-12" />
+                            <span className="text-2xl font-bold">
+                                {item.name}
+                            </span>
+                        </Link>
+                    </div>
+                ))}
             </nav>
         </header>
     );
