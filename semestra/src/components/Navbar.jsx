b20aaa67328b041543d5748100f042ef54ecad04
WenAlgo <cwu009@citymail.cuny.edu>
2025-04-15 09:49:20 -0400

    Install React Icons
    https://react-icons.github.io/react-icons

--- semestra/src/components/Navbar.jsx
+++ semestra/src/components/Navbar.jsx
@@ -1,20 +1,31 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import { GiThreeLeaves } from "react-icons/gi";
+import { TbCalendarTime } from "react-icons/tb";
+import { ImCalculator } from "react-icons/im";
+import { FaRegUser } from "react-icons/fa";
+
 const Navbar = () => {
     return (
         <header>
             <nav className="flex justify-between">
                 <div>
-                    <Link to="/">SEMESTRA</Link>
+                    <Link to="/">
+                        <GiThreeLeaves />
+                        <span>SEMESTRA</span>
+                    </Link>
                 </div>
                 <div>
+                    <TbCalendarTime />
                     <Link to="/classes">CLASSES</Link>
                 </div>
                 <div>
+                    <ImCalculator />
                     <Link to="/gpaCalculator">GPA Calculator</Link>
                 </div>
                 <div>
+                    <FaRegUser />
                     <Link to="/profile">Profile</Link>
                 </div>
             </nav>
