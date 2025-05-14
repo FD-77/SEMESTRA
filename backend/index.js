b84d53246c2b76ccd8c48778026a78103212f4c4
WenAlgo <cwu009@citymail.cuny.edu>
2025-05-14 17:48:42 -0400

    Connect the application to MongoDB

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,25 @@
 const express = require("express");
 const app = express();
+
+const mongoose = require("mongoose");
+
 const port = 3000;
 
-app.get("/", (req, res) => {
-    res.send("Hello World!");
-});
+async function main() {
+    await mongoose.connect(
+        "mongodb+srv://cwu009:kI8uCd5mf9NiPGrh@cluster0.hkqzwo5.mongodb.net/semestra?retryWrites=true&w=majority&appName=Cluster0"
+    );
+
+    app.get("/", (req, res) => {
+        res.send("Hello World!");
+    });
+}
+
+main()
+    .then(() => {
+        console.log("Mongodb connect successfully!");
+    })
+    .catch((err) => console.log(err));
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
