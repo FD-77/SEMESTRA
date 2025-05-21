4c7bd3dc61041185d2015f3acda63d0b11ee3201
sdemetriusjenkins <sdemetriusjenkins@gmail.com>
2025-05-20 22:01:56 -0400

    Added changes for deploying backend

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,13 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+    origin: [
+        'http://localhost:5173',
+        'https://semestra.vercel.app'
+    ],
+    credentials: true
+}));
 app.use(express.json());
 
 // Connect to MongoDB
